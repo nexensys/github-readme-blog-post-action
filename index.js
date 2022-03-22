984a4108e88be7cfd2f39fd1a44ad83c5436e19c
ErrorGamer2000 <error2000gamer@gmail.com>
2022-03-22 20:02:09 +0000

    Initial commit and work.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+import actions from "@actions/core";
+import github from "@actions/github";
+import loadMetaData from "./metaData.js";
+import fetch from "node-fetch";
 
+async function main() {
+  let feed = await fetch("https://dev.to/feed/codewithsadee");
+  let meta = await loadMetaData("https://dev.to/codewithsadee/how-to-build-audio-stream-app-landing-page-using-html-css-js-1960");
+  console.log(meta);
+}
+
+main();
---
+++ metaData.js
@@ -0,0 +1,24 @@
+import fetch from "node-fetch";
+import * as cheerio from "cheerio";
+
+/**
+ * 
+ * @param {string} url
+ */
+export default async function loadMetaData(url) {
+  try {
+    let res = await fetch(url);
+    let html = await res.text();
+    let $ = cheerio.load(html);
+    let elems = Object.entries($("meta[property ^= 'og:']")).filter(function ([key, value]) {
+      return !isNaN(Number(key));
+    }).map(function ([key, value]) {
+      return value;
+    })
+    return Object.fromEntries(elems.map(function (el) {
+      return [el.attribs.property.replace(/^og:/, ""), el.attribs.content];
+    }))
+  } catch {
+    return null;
+  }
+}
