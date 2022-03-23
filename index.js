2093751f841148c6c1c4c04d2a59868a7c61cdbe
ErrorGamer2000 <error2000gamer@gmail.com>
2022-03-22 18:18:55 -0600

    Updates and testing

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
-import actions from "@actions/core";
+import core from "@actions/core";
 import github from "@actions/github";
 import loadMetaData from "./metaData.js";
 import fetch from "node-fetch";
+import Parser from "rss-parser";
+import { fileTypeFromBuffer } from "file-type";
+import bufferToDataUrl from "buffer-to-data-url";
+const toDataURL = bufferToDataUrl.default;
+
+const parser = new Parser({
+  customFields: {
+    items: ["dc:creator", "creator"]
+  }
+});
+const maxItems = 5;
 
 async function main() {
-  let feed = await fetch("https://dev.to/feed/codewithsadee");
-  let meta = await loadMetaData("https://dev.to/codewithsadee/how-to-build-audio-stream-app-landing-page-using-html-css-js-1960");
-  console.log(meta);
+  let feed = await loadFeed("https://dev.to/feed/codewithsadee");
+  feed.items.splice(maxItems);
+  for (let post of feed.items) {
+    core.info(`Loading data for post: ${post.title ?? post.link}`);
+    let meta = await loadMetaData(post.link);
+    meta = Object.assign(
+      {
+        url: post.link,
+        title: post.title,
+        description: post.contentSnippet,
+        image: null,
+        date: new Date(post.isoDate)
+      },
+      meta
+    );
+    meta.categories = post.categories || null;
+    meta.image = await loadImage(meta);
+  }
+}
+
+async function loadFeed(url) {
+  core.info(`Loading feed data for: ${url}`);
+  return await parser.parseURL(url);
+}
+
+async function loadImage(meta) {
+  if (!meta.image) {
+    core.warning(`No thumbnail found!`);
+    return "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=";
+  } else {
+    core.info(`Loading thumbnail...`);
+    let res = await fetch(meta.image);
+    let arrayBuf = await res.arrayBuffer();
+    core.info("Converting thumbnail to data url...");
+    let buf = Buffer.from(arrayBuf);
+    let type = (await fileTypeFromBuffer(buf)).mime;
+    return await toDataURL(type, buf);
+  }
 }
 
 main();
--- metaData.js
+++ metaData.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import * as cheerio from "cheerio";
 
 /**
- * 
+ *
  * @param {string} url
  */
 export default async function loadMetaData(url) {
@@ -10,14 +10,20 @@ export default async function loadMetaData(url) {
     let res = await fetch(url);
     let html = await res.text();
     let $ = cheerio.load(html);
-    let elems = Object.entries($("meta[property ^= 'og:']")).filter(function ([key, value]) {
-      return !isNaN(Number(key));
-    }).map(function ([key, value]) {
-      return value;
-    })
-    return Object.fromEntries(elems.map(function (el) {
-      return [el.attribs.property.replace(/^og:/, ""), el.attribs.content];
-    }))
+    let elems = Object.entries(
+      $("meta[property ^= 'og:']:not([property='og:type'])")
+    )
+      .filter(function ([key, value]) {
+        return !isNaN(Number(key));
+      })
+      .map(function ([key, value]) {
+        return value;
+      });
+    return Object.fromEntries(
+      elems.map(function (el) {
+        return [el.attribs.property.replace(/^og:/, ""), el.attribs.content];
+      })
+    );
   } catch {
     return null;
   }
